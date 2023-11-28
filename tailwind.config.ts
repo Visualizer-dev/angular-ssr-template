4c78bc1a6702f3375e5027dea113dd7c37a466f9
KaliemSB <mycael590@gmail.com>
2023-11-28 00:33:06 +0000

    feat: added tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}', './src/index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [require('tailwindcss-animate')],
+};
